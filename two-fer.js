6e70841390bc66657888302f48e6e1e9ed654516
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-12 11:22:40 -0400

    added strict and solved first test

--- two-fer.js
+++ two-fer.js
@@ -1,11 +1,11 @@
+'use strict';
+
 var TwoFer = function () {};
 
 TwoFer.prototype.twoFer = function (who) {
-  // your code goes here
-  // You will have to use the parameter who
-  // in some way. In this example, it is just
-  // returned, but your solution will have to
-  // use a conditional.
+  if (who = 'undefined') {
+    who = 'One for you, one for me.'
+  }
   return who;
 };
 
--- two-fer.spec.js
+++ two-fer.spec.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var TwoFer = require('./two-fer');
 
